403a76d0a017ff13938a1ab49533abba25a21188
Duy <duyvu372001@gmail.com>
2024-05-04 08:16:31 +0900

    createHeader

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="project" element={<Project />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
