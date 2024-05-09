24db14afd4be553f4f57e62a69dfe731953f6832
Duy <duyvu372001@gmail.com>
2024-05-09 21:46:17 +0900

    dashboardPrivate

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import userReducer from "./user/userSlice";
 import themeReducer from "./theme/themSlice";
+import languageReducer from "./language/languageSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
   theme: themeReducer,
+  language: languageReducer,
 });
 
 const persistConfig = {
