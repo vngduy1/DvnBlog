d4016edb8f0f07a5ae1590d30d813b8e99142cf8
Duy <duyvu372001@gmail.com>
2024-05-11 23:28:16 +0900

    updateApi

--- api/src/app/controllers/userController.js
+++ api/src/app/controllers/userController.js
@@ -1,5 +1,84 @@
-const testUser = (req, res) => {
-  res.send("Test User");
+import { errorHandler } from "../../utils/error.js";
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+const getUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return next(errorHandler(400, "User not found!"));
+    }
+    const { password } = user;
+    res.status(201).json(password);
+  } catch (error) {
+    next(errorHandler(400, error.message));
+  }
 };
 
-export { testUser };
+const updateUser = async (req, res, next) => {
+  const { old_password, new_password, new_password_2 } = req.body;
+
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+  if (old_password || new_password) {
+    if (old_password === new_password) {
+      return next(
+        errorHandler(
+          400,
+          "The new password must not be the same as the old password"
+        )
+      );
+    }
+    if (new_password !== new_password_2) {
+      return next(errorHandler(400, "Passwords do not match!"));
+    }
+    if (req.body.new_password.length < 6) {
+      return next(errorHandler(411, "Password must be at least 6 characters"));
+    }
+    req.body.new_password = bcryptjs.hashSync(req.body.new_password, 10);
+  }
+
+  if (req.body.username) {
+    if (req.body.username.length < 6 || req.body.username.length > 20) {
+      return next(
+        errorHandler(411, "Username must be between 7band 20 characters")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "Username cannot only contain letters and numbers")
+      );
+    }
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.new_password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(errorHandler(500, error.message));
+  }
+};
+
+export { getUser, updateUser };
--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { testUser } from "../app/controllers/userController.js";
+import { getUser, updateUser } from "../app/controllers/userController.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/", testUser);
+router.get("/getUser/:userId", verifyToken, getUser);
+router.put("/update/:userId", verifyToken, updateUser);
 
 export default router;
