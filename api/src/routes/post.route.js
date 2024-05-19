90a79d89ab330ace285d744104dd45360a6cadaf
Duy <duyvu372001@gmail.com>
2024-05-19 10:56:50 +0900

    postsPage

--- api/src/routes/post.route.js
+++ api/src/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create } from "../app/controllers/postController.js";
+import { create, getPosts } from "../app/controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+router.get("/get-posts", getPosts);
 
 export default router;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 const flowbite = require("flowbite-react/tailwind");
+const scrollbar = require("tailwind-scrollbar");
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -6,5 +7,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin()],
+  plugins: [flowbite.plugin(), scrollbar],
 };
