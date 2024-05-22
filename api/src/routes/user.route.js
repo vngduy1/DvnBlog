36a35e2db54dab35b4c91f7701b67094c3881644
Duy <duyvu372001@gmail.com>
2024-05-22 22:56:30 +0900

    postAction

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getUser,
+  getUsers,
   updateUser,
   deleteUser,
   signOut,
@@ -10,6 +11,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get("/getUser/:userId", verifyToken, getUser);
+router.get("/get-users", verifyToken, getUsers);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/sign-out", verifyToken, signOut);
